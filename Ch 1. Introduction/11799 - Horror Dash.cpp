f2aade62fccb45251213637ee64cb3ff2ab37463
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-08-29 19:28:08 +0530

    Add a solution

---
+++ Ch 1. Introduction/11799 - Horror Dash.cpp
@@ -0,0 +1,22 @@
+// 11799 - Horror Dash
+/**
+ *  Author: DNS-404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int cases, k, max=0;
+  cin >> cases;
+  for(int i=0; i<cases; i++){
+    cin >> k;
+    int monsters[k];
+    for(int i=0; i<k; i++){
+      cin >> monsters[i];
+      if(max <= monsters[i])
+        max = monsters[i];
+    }
+    cout << "Case " << i+1 << ": " << max << endl;
+    max=0;
+  }
+  return 0;
+}
