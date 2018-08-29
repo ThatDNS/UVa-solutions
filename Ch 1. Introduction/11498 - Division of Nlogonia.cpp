64de9df2997e572fe2e0db1833900a2aa5fbe09d
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-08-29 18:22:54 +0530

    Update 11498 - Division of Nlogonia.cpp

--- Ch 1. Introduction/11498 - Division of Nlogonia.cpp
+++ Ch 1. Introduction/11498 - Division of Nlogonia.cpp
@@ -4,30 +4,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int k, n, m;
-    while(cin>>k && k!=0){
-        cin >> n >> m;
-        int arr[k][2];
-        for(int i=0; i<k; i++){
-            cin >> arr[i][0] >> arr[i][1];
-        }
-        for(int i=0; i<k; i++){
-            if(arr[i][0] == n || arr[i][1] == m){
-                cout << "divisa" << endl;
-            }else if(arr[i][0] > n){
-                if(arr[i][1] > m){
-                    cout << "NE" << endl;
-                }else{
-                    cout << "SE" << endl;
-                }
-            }else if(arr[i][0] < n){
-                if(arr[i][1] > m){
-                    cout << "NO" << endl;
-                }else{
-                    cout << "SO" << endl;
-                }
-            }
-        }
+  int k, n, m;
+  while(cin>>k && k!=0){
+    cin >> n >> m;
+    int arr[k][2];
+    for(int i=0; i<k; i++){
+      cin >> arr[i][0] >> arr[i][1];
     }
-    return 0;
+    for(int i=0; i<k; i++){
+      if(arr[i][0] == n || arr[i][1] == m){
+        cout << "divisa" << endl;
+      }else if(arr[i][0] > n){
+        if(arr[i][1] > m){
+          cout << "NE" << endl;
+        }else{
+          cout << "SE" << endl;
+        }
+      }else if(arr[i][0] < n){
+        if(arr[i][1] > m){
+          cout << "NO" << endl;
+        }else{
+          cout << "SO" << endl;
+        }
+      }
+    }
+  }
+  return 0;
 }
