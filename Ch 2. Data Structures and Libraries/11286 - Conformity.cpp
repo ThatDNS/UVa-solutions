5fe2c0f21ace6545f598e6908505eb817822641c
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-10-08 20:10:52 +0530

    11286 - Conformity

---
+++ Ch 2. Data Structures and Libraries/11286 - Conformity.cpp
@@ -0,0 +1,42 @@
+// Problem: 11286 - Conformity
+/**
+ *  Author: DNS-404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+bool isEqual(vector<int> a, vector<int> b);
+int main(){
+	int a, b, max, count=0;
+	vector<int> courses;
+	while(cin >> a && a != 0){
+		map<vector<int> , int> mapC;
+		max = INT_MIN;
+		count = 0;
+		for(int i=0; i<a; i++){
+			courses.clear();
+			for(int j=0; j<5; j++){
+				cin >> b;
+				courses.push_back(b);
+			}
+			sort(courses.begin(), courses.end());
+			if(!mapC.count(courses)){
+				mapC.insert(pair <vector<int>, int> (courses, 1));
+			}else{
+				auto find = mapC.find(courses);
+				find->second += 1;
+			}
+		}
+		map<vector<int> , int>::iterator it;
+		for(it = mapC.begin(); it != mapC.end(); ++it){
+			if(it->second > max){
+				max = it->second;
+				count = 0;
+				++count;
+			}else if(it->second == max){
+				++count;
+			}
+		}
+		cout << count*max << endl;
+	}
+	return 0;
+}
