eff6ac4e81b3ed5a8356e54d7bcad15fab6a166a
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-10-18 12:06:40 +0530

    11565 - Simple Equations

---
+++ Ch 3. Problem Solving Paradigms/11565 - Simple Equations.cpp
@@ -0,0 +1,29 @@
+// Problem: 11565 - Simple Equations
+/**
+ *  Author: DNS-404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n, a, b, c;
+	bool found=false;
+	cin >> n;
+	while(n--){
+		cin >> a >> b >> c;
+		found=false;
+		for(int x=-100; x<=100 && !found; x++){
+			for(int y=-100; y<=100 && !found; y++){
+				for(int z=-100; z<=100 && !found; z++){
+					if(x!=y && y!=z && z!=x && (x+y+z)==a && (x*y)*z==b && (x*x+y*y+z*z)==c){
+						cout << x << " " << y << " " << z << endl;
+						found=true;
+					}
+				}
+			}
+		}
+		if(!found){
+			cout << "No solution.\n";
+		}
+	}
+	return 0;
+}
