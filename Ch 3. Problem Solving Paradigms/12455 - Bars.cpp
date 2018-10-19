3ca2566422a116f9fdbf87a3560cafc3ddde68c3
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-10-19 15:42:47 +0530

    Add Bars

---
+++ Ch 3. Problem Solving Paradigms/12455 - Bars.cpp
@@ -0,0 +1,53 @@
+// Problem: 12455 - Bars
+/**
+ *  Author: DNS-404
+ **/
+#include <bits/stdc++.h>
+#define FOR0(i,n) for(int i=0;i<n;i++)
+#define FORl(i,l,n) for(int i=l;i<n;i++)
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+bool possible(int curr, vector<int> bars, int n, int sum);
+int main(){
+	short int t, n, p;
+	int temp;
+	vector<int> bars;
+	cin >> t;
+	while(t--){
+		bars.clear();
+		cin >> n;
+		cin >> p;
+		int dp[1010] = {}; // initialise to 0
+		dp[0] = 1;
+		for(int i=0; i<p; i++){
+			cin >> temp;
+			bars.push_back(temp);
+			for(int j=n-temp; j>=0; j--){
+				if(dp[j] && !dp[j+temp]){
+					dp[j+temp] = 1;
+				}
+			}
+		}
+		if(dp[n])
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+	return 0;
+}
+
+// Recursive (very slow)
+bool possible(int curr, vector<int> bars, int n, int sum){
+	if(sum == n)
+		return true;
+	if(sum > n)
+		return false;
+	for(int i=curr; i<bars.size()-1; i++){
+		if(possible(i+1, bars, n, sum)){
+			return true;
+		}else if(possible(i+1, bars, n, sum+bars[i])){
+			return true;
+		}
+	}
+	return false;
+}
