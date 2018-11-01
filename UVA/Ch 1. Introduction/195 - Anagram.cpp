5e7654a1547fcc0b02859142f93cf0c8bec38355
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-11-02 00:37:56 +0530

    195 - Anagram

---
+++ UVA/Ch 1. Introduction/195 - Anagram.cpp
@@ -0,0 +1,39 @@
+// Problem: 195 - Anagram
+/**
+ *	Author: DNS-404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define INF 1e9 // 10^9
+bool Compare(char a, char b);
+int main(){
+	int n;
+	string s;
+	cin >> n;
+	while(n--){
+		cin >> s;
+		permute(s);
+	}
+	return 0;
+}
+
+void permute(string s){
+	sort(s.begin(), s.end(), Compare);
+	cout << s << endl;
+	while(next_permutation(s.begin(), s.end(), Compare)){
+		cout << s << endl;
+	}
+}
+
+bool Compare(char a, char b){
+	if(tolower(a) == tolower(b))
+		return a<b;
+	return tolower(a)<tolower(b);
+}
