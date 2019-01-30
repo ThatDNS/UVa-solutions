503afffa1e2396f518d57372b9ce5cbfb451f241
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-01-30 21:41:12 +0530

    1225 - Digit Counting

---
+++ UVA/Ch 5. Mathematics/1225 - Digit Counting.cpp
@@ -0,0 +1,71 @@
+// Problem: 1225 - Digit Counting
+
+/**
+ *	Author: DNS404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+/*---------------Bitmask---------------*/
+#define setBit(S, pos) (S |= (1 << pos))
+#define resetBit(S, pos) (S &= ~(1 << pos))
+#define checkBit(S, pos) (S & (1 << pos))
+#define toggleBit(S, pos) (S ^= (1 << pos))
+#define lowBit(S) (S & (-S)) //get least bit that is 1
+/*-------------------------------------*/
+inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define For0(i,n) for(int i=0; i<n; i++)
+#define For(i,a,b) for(int i=a; i<=b; i++)
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(0);
+#define disableAutoFlush cin.tie(NULL); cout.tie(NULL);
+#define SWAP(a,b) a=a^b, b=a^b, a=a^b
+
+int main(){
+	int n, tNo, test;
+	string final = "", temp;
+	int arr[12];
+	cin >> test;
+	while(test--){
+		cin >> n;
+		final = "";
+		for(int i=1; i<=n; i++){
+			temp = to_string(i);
+			final += temp;
+		}
+		clr(arr,0);
+		for(int i=0; i<final.length(); i++){
+			temp = final.substr(i, 1);
+			tNo = std::stoi(temp);
+			++arr[tNo];
+		}
+		for(int i=0; i<=9; i++){
+			if(i<9)
+				pfs(arr[i]);
+			else
+				printf("%d\n", arr[i]);
+		}
+	}
+	return 0;
+}
+
+
+
