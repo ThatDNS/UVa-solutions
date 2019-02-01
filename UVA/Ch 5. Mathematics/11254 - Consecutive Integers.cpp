3e831f0da8ce67cf5e34e69b2b416f4343d7a038
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-02-01 16:09:09 +0530

    11254 - Consecutive Integers

---
+++ UVA/Ch 5. Mathematics/11254 - Consecutive Integers.cpp
@@ -0,0 +1,54 @@
+// Problem: 11254 - Consecutive Integers
+
+/**
+ *	Author: DNS404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+/*---------------Bitmask---------------*/
+#define setBit(S, pos) (S |= (1 << pos))
+#define resetBit(S, pos) (S &= ~(1 << pos))
+#define checkBit(S, pos) (S & (1 << pos))
+#define toggleBit(S, pos) (S ^= (1 << pos))
+#define lowBit(S) (S & (-S)) //get least bit that is 1
+/*-------------------------------------*/
+inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define For0(i,n) for(int i=0; i<n; i++)
+#define For(i,a,b) for(int i=a; i<=b; i++)
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(0);
+#define disableAutoFlush cin.tie(NULL); cout.tie(NULL);
+#define SWAP(a,b) a=a^b, b=a^b, a=a^b
+
+int main(){
+	double n, a;
+	ll r;
+	while(cin>>n && n!=-1){
+		for(r = 2*sqrt(n); r>=1; r--){
+			a = (2*n + r - pow(r, 2))/(2*r);
+			if(a == (int)a && a>0)
+				break;
+		}
+		printf("%d = %d + ... + %d\n", (int)n, (int)a, (int)(a+r-1));
+	}
+	return 0;
+}
