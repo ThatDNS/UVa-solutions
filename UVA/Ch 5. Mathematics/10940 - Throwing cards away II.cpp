060b5eecbf505bc54e90b39c700c4b2360828bdf
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-02-01 11:15:45 +0530

    10940 - Throwing cards away II

---
+++ UVA/Ch 5. Mathematics/10940 - Throwing cards away II.cpp
@@ -0,0 +1,60 @@
+// Problem: 10940 - Throwing cards away II
+/**
+ *	Author: DNS404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+/*---------------Bitmask---------------*/
+#define setBit(S, pos) (S |= (1 << pos))
+#define resetBit(S, pos) (S &= ~(1 << pos))
+#define checkBit(S, pos) (S & (1 << pos))
+#define toggleBit(S, pos) (S ^= (1 << pos))
+#define lowBit(S) (S & (-S)) //get least bit that is 1
+/*-------------------------------------*/
+inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define For0(i,n) for(int i=0; i<n; i++)
+#define For(i,a,b) for(int i=a; i<=b; i++)
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(0);
+#define disableAutoFlush cin.tie(NULL); cout.tie(NULL);
+#define SWAP(a,b) a=a^b, b=a^b, a=a^b
+
+int main(){
+	int n, count=1, temp, iter;
+	vi ans;
+	ans.pb(0);
+	ans.pb(1);
+	for(; ans.size() <= 500000; ){
+		temp = count;
+		iter = 2;
+		while(temp--){
+			ans.pb(iter);
+			iter = iter+2;
+		}
+		count = count*2;
+	}
+	while(cin>>n && n){
+		cout << ans[n] << endl;
+	}
+	return 0;
+}
+
