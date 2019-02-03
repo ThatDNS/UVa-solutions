5b4f471cd31e58c5322aeb90c6fcb4b15edf1697
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-02-03 14:55:01 +0530

    264 - Count on Cantor

---
+++ UVA/Ch 5. Mathematics/264 - Count on Cantor.cpp
@@ -0,0 +1,67 @@
+// Problem: 264 - Count on Cantor
+
+/**
+ *	Author: DNS404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+/*---------------Bitmask---------------*/
+#define setBit(S, pos) (S |= (1 << pos))
+#define resetBit(S, pos) (S &= ~(1 << pos))
+#define checkBit(S, pos) (S & (1 << pos))
+#define toggleBit(S, pos) (S ^= (1 << pos))
+#define lowBit(S) (S & (-S)) //get least bit that is 1
+/*-------------------------------------*/
+inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define For0(i,n) for(int i=0; i<n; i++)
+#define For(i,a,b) for(int i=a; i<=b; i++)
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(0);
+#define disableAutoFlush cin.tie(NULL); cout.tie(NULL);
+#define SWAP(a,b) a=a^b, b=a^b, a=a^b
+
+int main(){
+	vi num;
+	vi den;
+	int sum=2, n;
+	bool turns=false;
+	for(; num.size()<10000005;){
+		if(turns){
+			for(int a=1; a<=sum-1; a++){
+				num.pb(a);
+				den.pb(sum-a);
+			}
+		}else{
+			for(int a=sum-1; a>=1; a--){
+				num.pb(a);
+				den.pb(sum-a);
+			}
+		}
+		turns = !turns;
+		++sum;
+	}
+	while(cin >> n){
+		printf("TERM %d IS %d/%d\n", n, num[n-1], den[n-1]);
+	}
+	return 0;
+}
+
