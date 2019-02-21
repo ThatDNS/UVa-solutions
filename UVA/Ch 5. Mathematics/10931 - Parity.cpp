d2b09ae7c59032241d46ad12ed5bf889776bd86b
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-02-21 11:58:17 +0530

    10931 - Parity.cpp

---
+++ UVA/Ch 5. Mathematics/10931 - Parity.cpp
@@ -0,0 +1,54 @@
+// Problem: 10931 - Parity
+
+/**
+ *  Author: DNS404
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+/*---------------Bitmask---------------*/
+#define setBit(S, pos) (S |= (1 << pos))
+#define resetBit(S, pos) (S &= ~(1 << pos))
+#define checkBit(S, pos) (S & (1 << pos))
+#define toggleBit(S, pos) (S ^= (1 << pos))
+#define lowBit(S) (S & (-S)) //get least bit that is 1
+/*-------------------------------------*/
+inline long long  MAX2(long long  a, long long int b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(0);
+#define disableAutoFlush cin.tie(NULL); cout.tie(NULL);
+#define SWAP(a,b) a=a^b, b=a^b, a=a^b
+
+int main(){
+  int I, no, from;
+  while(cin >> I && I){
+    bitset<32> a(I);
+    no = a.count();
+    for(int i=a.size()-1; i>=0; i--){
+      if(a.test(i)){
+        from = i;
+        break;
+      }
+    }
+    printf("The parity of ");
+    for(int i=from; i>=0; i--)
+      cout << a.test(i);
+    printf(" is %d (mod 2).\n", no);
+  }
+  return 0;
+}
+
