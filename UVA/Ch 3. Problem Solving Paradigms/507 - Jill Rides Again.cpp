8301a719a11c442b5564f9147832ac4f66c0c43c
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-05-10 22:25:31 +0530

    Added another solution to 507 - Jill Rides Again

--- UVA/Ch 3. Problem Solving Paradigms/507 - Jill Rides Again.cpp
+++ UVA/Ch 3. Problem Solving Paradigms/507 - Jill Rides Again.cpp
@@ -32,3 +32,66 @@ int main(){
   }
   return 0;
 }
+
+// Solved again. Solution:
+
+#include <bits/stdc++.h>
+using namespace std;
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+typedef pair<int, int> ii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define PI 3.14159265
+#define EPS 1e-9 // 10^-9
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define INF 1e9 // 10^9
+#define MOD 1000000007
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(false);
+#define disableAutoFlush cin.tie(NULL);
+
+int main(){
+  disableSync
+  int t, n, sum, ans, x, y, temp, count=0;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    //cout << "n is " << n << endl;
+    int arr[n+2];
+    x = 0; y = 0; temp = 0;
+    for(int i=0; i<n-1; i++)
+      cin >> arr[i];
+
+    // printf("Array is: \n");
+    // for(int i=0; i<n-1; i++)
+    //   cout << arr[i] << " ";
+    // cout << endl;
+
+    sum=0; ans=INT_MIN;
+    for(int i=0; i<n-1; i++){
+      //cout << "sum is " << sum << endl;
+      sum += arr[i];
+      //cout << "sum is " << sum << endl;
+      if((sum > ans) || (sum == ans && abs(y-x) < abs(i-temp))){
+        y = i;
+        x = temp;
+        ans = sum;
+      }
+      if(sum < 0){
+        sum = 0;
+        temp = i+1;
+      }
+    }
+    //cout << ans << endl;
+    if(ans < 0){
+      printf("Route %d has no nice parts\n", ++count);
+    }else{
+      printf("The nicest part of route %d is between stops %d and %d\n", ++count, x+1, y+2);
+    }
+  }
+  return 0;
+}
+
