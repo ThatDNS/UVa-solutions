757be5f43455ee76ecae59839927a25b853b041d
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-06-09 18:14:39 +0530

    10219 - Find the ways

---
+++ UVA/Ch 5. Mathematics/10219 - Find the ways.cpp
@@ -0,0 +1,36 @@
+// Problem: 10219 - Find the ways !
+#include <bits/stdc++.h>
+using namespace std;
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+#define vi vector<int>
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define For0(i,n) for(int i=0; i<n; i++)
+#define ForF(i,a,b) for(int i=a; i<=b; i++)
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(false);
+#define disableAutoFlush cin.tie(NULL);
+#define MOD 1000000007
+
+int main(){
+  ll n, k;
+  double digits;
+  while(cin >> n >> k){
+    digits = 0;
+    if(k > n/2)
+      k = n-k;
+    for(ll i=n; i>=n-k+1; i--)
+      digits += log10(i);
+    for(ll i=1; i<=k; i++)
+      digits -= log10(i);
+    printf("%0.lf\n", floor(digits)+1);
+  }
+  return 0;
+}
