6430d5cc1ce33a484d6df4ca46be0b26ea9cb4cf
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-06-11 14:49:57 +0530

    991 - Safe Salutations

---
+++ UVA/Ch 5. Mathematics/991 - Safe Salutations.cpp
@@ -0,0 +1,38 @@
+// Problem: 991 - Safe Salutations
+
+#include <bits/stdc++.h>
+using namespace std;
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+#define vi vector<int>
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define For0(i,n) for(int i=0; i<n; i++)
+#define ForF(i,a,b) for(int i=a; i<=b; i++)
+#define TheCase(i) printf("Case %d: ", i);
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(false);
+#define disableAutoFlush cin.tie(NULL);
+#define MOD 1000000007
+
+int main(){
+  int n, flag=0;
+  int cat[12];
+  cat[0] = 1;
+  cat[1] = 1;
+  for(int i=2; i<=10; i++){
+    cat[i] = (((2*i)*((2*i)-1))*cat[i-1])/((i+1)*i);
+  }
+  while(cin >> n){
+    if(flag==1) printf("\n");
+    else flag = 1;
+    cout << cat[n] << endl;
+  }
+  return 0;
+}
