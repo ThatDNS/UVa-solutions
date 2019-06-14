28874a3dde52e2b0ff89b49b7a289a719718f829
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2019-06-14 13:32:29 +0530

    10673 - Play with Floor and Ceil

---
+++ UVA/Ch 5. Mathematics/10673 - Play with Floor and Ceil.cpp
@@ -0,0 +1,53 @@
+// Problem: 10673 - Play with Floor and Ceil
+
+#include <bits/stdc++.h>
+using namespace std;
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+#define vi vector<int>
+#define sf(n) scanf("%d", &n)
+#define sff(a,b) scanf("%d %d", &a, &b)
+#define sfff(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define pfn(n) printf("%d\n", n)
+#define pfs(n) printf("%d ", n)
+#define For0(i,n) for(int i=0; i<n; i++)
+#define ForF(i,a,b) for(int i=a; i<=b; i++)
+#define TheCase(i) printf("Case %d: ", i);
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define clr(x,a) memset(x,a,sizeof(x))
+#define disableSync ios_base::sync_with_stdio(false);
+#define disableAutoFlush cin.tie(NULL);
+
+int x, y, d;
+void ExtendedEuclid(int a, int b);
+
+int main(){
+  int t, a, b;
+  ll m, n;
+  sf(t);
+  while(t--){
+    sff(a, b);
+    ExtendedEuclid(a/b, (a/b)+(a%b!=0));
+    m = (x*a)/d;
+    n = (y*a)/d;
+    printf("%lld %lld\n", m, n);
+  }
+  return 0;
+}
+
+void ExtendedEuclid(int a, int b){
+  if(b==0){
+    x = 1;
+    y = 0;
+    d = a;
+    return;
+  }
+  ExtendedEuclid(b, a%b);
+  int x1 = y;
+  int y1 = x - (a/b)*y;
+  x = x1;
+  y = y1;
+}
+
